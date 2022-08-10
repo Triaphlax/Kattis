0aa16d11de4b923d602970acb58a1dbe1ac2e88c
Rolf Plagmeijer <sausage948@gmail.com>
2022-08-10 18:33:13 +0200

    Getting into Max Flow

--- Python/Snippets/BFS.py
+++ Python/Snippets/BFS.py
@@ -11,13 +11,3 @@ def bfs_iterative(graph, start):
                 stack.append(neighbor)
 
     return (discovered)
-
-
-allNodesToVisit = set(range(nofNodes))
-graph = {new_list: [] for new_list in range(nofNodes)}
-
-for road in range(nofRoads):
-    road = list(map(int, sys.stdin.readline().strip().split(' ')))
-    for endpointIndex in [0,1]:
-        otherEndpointIndex = (endpointIndex + 1) % 2
-        graph[road[endpointIndex]].append(road[otherEndpointIndex])
--- Python/Snippets/DFS.py
+++ Python/Snippets/DFS.py
@@ -12,7 +12,7 @@ def dfs_iterative(graph, start):
 
     return (path)
 
-# def bfs_iterative(graph, start):
+# def dfs_iterative(graph, start):
 #     stack = [start]
 #     discovered = set()
 
@@ -25,14 +25,3 @@ def dfs_iterative(graph, start):
 #                 stack.append(neighbor)
 
 #     return (discovered)
-    
-
-
-allNodesToVisit = set(range(nofNodes))
-graph = {new_list: [] for new_list in range(nofNodes)}
-
-for road in range(nofRoads):
-    road = list(map(int, sys.stdin.readline().strip().split(' ')))
-    for endpointIndex in [0,1]:
-        otherEndpointIndex = (endpointIndex + 1) % 2
-        graph[road[endpointIndex]].append(road[otherEndpointIndex])
