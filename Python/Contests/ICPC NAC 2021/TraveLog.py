ad238405cefcb20b62804d467629401a6bdd5d5c
Triaphlax <RolfP@Ortec.Com>
2022-09-20 17:47:25 +0200

    Some more ICPC NAC 2021

--- Python/Contests/ICPC NAC 2021/TraveLog.py
+++ Python/Contests/ICPC NAC 2021/TraveLog.py
@@ -1,18 +1,25 @@
+import sys
 import queue
+
+#############################
+sys.stdin = open('TEST/TestFile.txt', 'r')
+#############################
 
 
 class Graph:
     def __init__(self, source, nodesList):
         self.nodeDict = {}
         self.source = source
-        self.shortestDistFromSource = []
+        self.shortestDistFromSource = {}
+        self.nodesList = nodesList
         self.nofNodes = len(nodesList)
         for nodeNumber in nodesList:
             self.addNode(nodeNumber)
 
     def resetShortestPaths(self):
         self.nofNodes = len(self.nodeDict)
-        self.shortestDistFromSource = [float("inf")] * self.nofNodes
+        self.shortestDistFromSource = {
+            key: float("inf") for key in self.nodesList}
 
     def addNode(self, nodeNumber):
         self.node = Node(nodeNumber)
@@ -27,18 +34,28 @@ class Graph:
             return float("inf")
         return self.shortestDistFromSource[nodeNumber]
 
-    def dijkstra(self):
+    def dijkstra(self, fullDataLog):
         self.resetShortestPaths()
 
         self.shortestDistFromSource[self.source] = 0
 
         pq = queue.PriorityQueue()
-        pq.put((0, self.source))
+        pq.put((0, self.source, [], 0))
 
         while not pq.empty():
-            (currentDistance, currentNodeNumber) = pq.get()
+            currentDistance, currentNodeNumber, currentRoute, currentDataIndex = pq.get()
+
+            if currentNodeNumber == self.nofNodes:
+                results.append(currentRoute + [currentNodeNumber])
+                continue
+
             currentNode = self.nodeDict[currentNodeNumber]
             currentNode.setVisited()
+
+            if currentDataIndex < len(fullDataLog):
+                nextPoint = fullDataLog[currentDataIndex]
+            else:
+                nextPoint = float("inf")
 
             for neighborNodeNumber in currentNode.neighbors.keys():
                 neighborNode = self.nodeDict[neighborNodeNumber]
@@ -47,9 +64,12 @@ class Graph:
                         neighborNodeNumber)
                     oldDistance = self.shortestDistFromSource[neighborNodeNumber]
                     newDistance = currentDistance + travelDistance
-                    if newDistance < oldDistance:
+                    if newDistance <= oldDistance and newDistance <= nextPoint:
                         self.shortestDistFromSource[neighborNodeNumber] = newDistance
-                        pq.put((newDistance, neighborNodeNumber))
+                        if newDistance == nextPoint:
+                            currentDataIndex += 1
+                        pq.put((newDistance, neighborNodeNumber,
+                               currentRoute + [currentNodeNumber], currentDataIndex))
 
 
 class Node:
@@ -66,3 +86,32 @@ class Node:
 
     def getDistanceToNeighbor(self, neighborNumber):
         return self.neighbors[neighborNumber]
+
+
+nofCities, nofRoads, nofData = list(
+    map(int, sys.stdin.readline().strip().split(' ')))
+
+graph = Graph(1, range(1, nofCities+1))
+
+for _ in range(nofRoads):
+    fromNumber, toNumber, length = list(
+        map(int, sys.stdin.readline().strip().split(' ')))
+    graph.addEdge(fromNumber, toNumber, length)
+
+corruptedLog = []
+for _ in range(nofData):
+    dataPoint = int(sys.stdin.readline().strip())
+    corruptedLog.append(dataPoint)
+corruptedLog.sort()
+
+results = []
+graph.dijkstra(corruptedLog)
+
+if len(results) == 0:
+    print(0)
+elif len(results) > 1:
+    print(1)
+else:
+    print(len(results[0]))
+    for i in range(len(results[0])):
+        print(results[0][i])
