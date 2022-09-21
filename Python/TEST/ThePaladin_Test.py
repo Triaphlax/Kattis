1f1fc132d2fa8f5bbfcfa2e6fdd63b3f15579e4c
Rolf Plagmeijer <sausage948@gmail.com>
2022-09-21 22:54:17 +0200

    Add Paladin tests

---
+++ Python/TEST/ThePaladin_Test.py
@@ -0,0 +1,14 @@
+import random
+
+f = open("TEST/ThePaladin_Test.txt", 'a')
+
+letters = "abcdefghijklmnopqrstuvwxyz"
+nofPairs = 676
+chainLength = 100
+f.write(str(nofPairs) + ' ' + str(chainLength) + '\n')
+for i in range(0, nofPairs):
+    first = i // 26
+    second = i % 26
+    f.write(str(letters[first]) + str(letters[second]) + ' ' + str(random.randint(1, 100)) + '\n')
+
+f.close()
